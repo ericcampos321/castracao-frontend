2cc740114a43c003017d512c6f0eb3f3a392044c
ericcampos321 <ericaguiar_@hotmail.com>
2024-10-08 17:25:37 -0300

    feature/pages-login

---
+++ frontend/src/pages/login/index.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import img from "../../assets/images/logo.jpeg";
+
+import "./style.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="background-login">
+      <div class="form-control">
+        <div className="box-image">
+          <img src={img} alt="logo" />
+        </div>
+        <form action="">
+          <div class="input-field">
+            <input
+              required
+              className="input"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label class="label" for="input">
+              Enter Email
+            </label>
+          </div>
+          <div class="input-field">
+            <input
+              required
+              class="input"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label class="label" for="input">
+              Enter Password
+            </label>
+          </div>
+          <div className="box-submit-login">
+            <button class="submit-btn">Sign In</button>
+          </div>
+          
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
